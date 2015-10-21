dbbb2d4705324b14f993acb1964004e988a70dae
Lokesh <lokesh.gahlot@wellsfargo.com>
2015-10-21 06:06:31 -0700

    Detete unwated files... deleted:    src/gulp/gulpfile.js deleted:    src/gulp/package.json deleted:    src/gulp/server/index.js deleted:    src/gulp/tasks/clean.js deleted:    src/gulp/tasks/config.js deleted:    src/gulp/tasks/express.js deleted:    src/gulp/tasks/index.js deleted:    src/gulp/tasks/jade.js deleted:    src/gulp/tasks/sass.js deleted:    src/gulp/tasks/script.js

--- src/gulp/gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var argv = require('yargs').argv;
-
-process.env.NODE_ENV = argv.env || process.env.NODE_ENV || 'dev';
-process.env.NODE_PORT = argv.env || process.env.NODE_PORT || '8080';
-process.env.NODE_PROXY_PORT = argv.env || process.env.NODE_PROXY_PORT || '3030';
-
-require('./tasks')();
